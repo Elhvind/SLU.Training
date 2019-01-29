0e0c66610be68bd6ad8848e35f9152e52ce61baa
Søren Lund <lund14@gmail.com>
2019-01-29 23:18:34 +0100

    Set menu items active

---
+++ SLU.Training/SLU.Training/Extensions/HtmlHelperExtensions/IsMenuActive.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+
+namespace SLU.Training.Extensions.HtmlHelperExtensions
+{
+    public static partial class HtmlHelperExtension
+    {
+        public static string IsMenuActive(this HtmlHelper html, string controllers = "", string actions = "", string cssClass = "active")
+        {
+            var viewContext = html.ViewContext.Controller.ControllerContext.IsChildAction
+                ? html.ViewContext.ParentActionViewContext
+                : html.ViewContext;
+
+            var routeValues = viewContext.RouteData.Values;
+            string currentAction = routeValues["action"].ToString();
+            string currentController = routeValues["controller"].ToString();
+
+            if (string.IsNullOrEmpty(actions))
+                actions = currentAction;
+
+            if (string.IsNullOrEmpty(controllers))
+                controllers = currentController;
+
+            string[] acceptedActions = actions.Trim().Split(',').Distinct().ToArray();
+            string[] acceptedControllers = controllers.Trim().Split(',').Distinct().ToArray();
+
+            return acceptedActions.Contains(currentAction) && acceptedControllers.Contains(currentController)
+               ? cssClass
+               : string.Empty;
+        }
+    }
+}
